016026b508a49b27280850bc6377bad9e4a5d5d2
Aadityarlobo <aadityalobo@gmail.com>
2023-11-09 15:21:18 +0530

    A

--- src/index.js
+++ src/index.js
@@ -8,28 +8,3 @@ if('serviceWorker' in navigator) {
     })
   }
 
-
-  // Video sources and index
-const videos = ["video1.mp4", "video2.mp4", "video3.mp4"];
-let currentIndex = 0;
-
-// Like button click handler
-const likeButton = document.getElementById("like-button");
-likeButton.addEventListener("click", () => {
-    // Handle the like action here, e.g., increment a like count
-    // You can also save this information on the server
-    alert("Video Liked!");
-});
-
-// Timer function to switch to the next video after 24 hours
-function startVideoRotation() {
-    setInterval(() => {
-        currentIndex = (currentIndex + 1) % videos.length;
-        const videoPlayer = document.getElementById("video-player");
-        videoPlayer.src = videos[currentIndex];
-        videoPlayer.load();
-    }, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
-}
-
-// Initialize the video rotation timer
-startVideoRotation();
